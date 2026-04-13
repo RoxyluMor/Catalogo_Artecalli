e5b5a633a677e3f58c25b45a71090babc7dd2777
roxana <roxanamorales25313@gmail.com>
2026-04-12 20:56:58 -0600

    Mejoras en vistas y diseños

--- artecalli/views/layouts/admin.php
+++ artecalli/views/layouts/admin.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $pageTitle ?? 'Panel de Administrador'; ?> - Artecalli</title>
+    <title><?php echo $pageTitle ?? 'Panel del Administrador'; ?> - Artecalli</title>
     <link rel="icon" href="<?php echo $basePath; ?>/assets/images/logo.png">
     
     <!-- Bootstrap CSS -->
